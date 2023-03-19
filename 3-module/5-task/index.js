906b0a21fd74fce97fef079e8e6c3a3c8e200319
Anna Kuzina <anna.derey@mail.ru>
2023-03-19 19:53:50 +0700

    Update index.js

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
   // ваш код...
+  let arr = str.split(' ').filter(elm => !isNaN(parseFloat(elm)));
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  return {min, max};
 }
+
+const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
+console.log(getMinMax(inputData));
