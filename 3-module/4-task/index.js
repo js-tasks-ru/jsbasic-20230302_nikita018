adb878083ff1e298828c81272811be3c715eb78b
Brandine <anna.derey@mail.ru>
2023-04-01 00:53:52 +0700

    Я попыталась исправить задание, но конфликта не заметила

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,15 +1,30 @@
+let user1 = {
+  balance: "$1,825.65",
+  picture: "https://placehold.it/32x32",
+  age: 21,
+  name: "Golden Branch",
+  gender: "male",
+  greeting: "Hello, Golden Branch! You have 7 unread messages.",
+  favouriteFruit: "banana",
+};
+
+let user2 = {
+  balance: "$1,490.15",
+  picture: "https://placehold.it/32x32",
+  age: 55,
+  name: "Duncan Randall",
+  gender: "male",
+  greeting: "Hello, Duncan Randall! You have 10 unread messages.",
+  favouriteFruit: "apple",
+};
+
+let users = [user1, user2];
+
 function showSalary(users, age) {
   // ваш код...
-  let eligibleUsers = users.filter(user => user.age <= age);
-  let formattedUsers = eligibleUsers.map(user => `${user.name}, ${user.balance}`);
-  return formattedUsers.join('\n');
+  let filteredUsers = users.filter((user) => user.age <= age);
+  let resultArray = filteredUsers.map(
+    (user) => `${user.name}, ${user.balance}`
+  );
+  return resultArray.join("\n");
 }
-
-let users = [
-  { "balance": "$1,825.65", "picture": "https://placehold.it/32x32", "age": 21, "name": "Golden Branch", "gender": "male", "greeting": "Hello, Golden Branch! You have 7 unread messages.", "favouriteFruit": "banana" },
-  { "balance": "$1,490.15", "picture": "https://placehold.it/32x32", "age": 28, "name": "Duncan Randall", "gender": "male", "greeting": "Hello, Duncan Randall! You have 3 unread messages.", "favouriteFruit": "banana" },
-];
-
-let maxAge = 26;
-let result = showSalary(users, maxAge);
-console.log(result);
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,7 +5,3 @@ function getMinMax(str) {
   let max = Math.max(...arr);
   return {min, max};
 }
-
-const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
-
-console.log(getMinMax(inputData));
