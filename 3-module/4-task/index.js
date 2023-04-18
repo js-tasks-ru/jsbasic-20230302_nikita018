15c208ef256894c5dfcb4b46cd54ede5e04e9db9
Brandine <anna.derey@mail.ru>
2023-04-18 10:37:47 +0700

    Update index.js

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,25 +1,3 @@
-let user1 = {
-  balance: "$1,825.65",
-  picture: "https://placehold.it/32x32",
-  age: 21,
-  name: "Golden Branch",
-  gender: "male",
-  greeting: "Hello, Golden Branch! You have 7 unread messages.",
-  favouriteFruit: "banana",
-};
-
-let user2 = {
-  balance: "$1,490.15",
-  picture: "https://placehold.it/32x32",
-  age: 55,
-  name: "Duncan Randall",
-  gender: "male",
-  greeting: "Hello, Duncan Randall! You have 10 unread messages.",
-  favouriteFruit: "apple",
-};
-
-let users = [user1, user2];
-
 function showSalary(users, age) {
   return users
     .filter((user) => user.age <= age)
